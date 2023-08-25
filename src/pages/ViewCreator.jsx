13707b26d58c8edc2cc8e6e2c600aa742c6ef424
dive0 <zhengshiwei5@gmail.com>
2023-08-25 14:20:47 -0500

    Style creator info page

--- src/pages/ViewCreator.jsx
+++ src/pages/ViewCreator.jsx
@@ -8,19 +8,26 @@ const ViewCreator = (props) => {
   const creator = creators.filter((item) => item.id == id)[0];
 
   return (
-    <div>
+    <div className="flex items-center flex-col text-2xl text-white space-y-3">
+      {creator.imageURL && creator.imageURL.length != 0 ? (
+        <img src={creator.imageURL} alt="image" className="h-52 w-52 rounded-lg"/>
+      ) : (
+        <img
+          src="https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg"
+          alt="White Background"
+          className="h-52 w-52 rounded-lg"
+        />
+      )}
+
       <h1>Creator: {creator.name}</h1>
       <p>URL: {creator.url}</p>
       <p>Description: {creator.description}</p>
-      {creator.imageURL != "" ? (
-        <img src={creator.imageURL} alt="image" />
-      ) : (<div></div>)}
 
       <Link to={`/${creator.id}/edit`}>
-        <button>Edit Creator</button>
+        <button className="bg-blue-500 text-lg p-1 rounded hover:bg-blue-300 transition text-black my-5">Edit Creator</button>
       </Link>
     </div>
-  )
+  );
 };
 
 export default ViewCreator;
